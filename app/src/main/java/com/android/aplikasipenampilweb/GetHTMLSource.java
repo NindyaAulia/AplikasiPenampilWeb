28190236ac5422ce0e78035ea3a6e6c49407c7b9
NindyaAulia <nindya03@gmail.com>
2018-01-09 16:10:44 +0700

    Initial commit

---
+++ app/src/main/java/com/android/aplikasipenampilweb/GetHTMLSource.java
@@ -0,0 +1,57 @@
+package com.android.aplikasipenampilweb;
+
+import android.content.Context;
+import android.support.v4.content.AsyncTaskLoader;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+
+public class GetHTMLSource extends AsyncTaskLoader<String>{
+    private String url_link;
+
+    public GetHTMLSource(Context context, String url_link){
+        super(context);
+        this.url_link = url_link;
+    }
+
+    @Override
+    protected void onStartLoading(){forceLoad();}
+
+    @Override
+    public String loadInBackground(){
+        InputStream in;
+
+        try{
+            URL url = new URL(url_link);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setReadTimeout(10000);
+            connection.setConnectTimeout(20000);
+            connection.setRequestMethod("GET");
+            connection.connect();
+
+            in = connection.getInputStream();
+
+            BufferedReader buff = new BufferedReader(new InputStreamReader(in));
+            StringBuilder sb = new StringBuilder();
+            String line = "";
+
+            while ((line = buff.readLine()) !=null){
+                sb.append(line + "\n");
+            }
+            buff.close();
+            in.close();
+
+            return sb.toString();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "Errors, Try change protocol to http or https";
+    }
+}
+
